1fccc18aba3241b4cbda0580e32ef93115400643
oluwabukola2610 <roheemohmudashir@gmail.com>
2023-08-08 18:02:26 +0100

    new changes

--- src/component/Social.jsx
+++ src/component/Social.jsx
@@ -40,7 +40,7 @@ const Social = () => {
           Resume <BsFillPersonLinesFill size={25} />
         </>
       ),
-      href: "/public/ROHEEMOH_MUDASHIR_Resume.pdf",
+      href: "/src/assets/ROHEEMOH_MUDASHIR_Resume.pdf",
       download: true,
       style: "rounded-br-md",
     },
