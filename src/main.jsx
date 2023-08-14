fe7d0a1433d8d98d91d4a00baef5385d966335da
oluwabukola2610 <roheemohmudashir@gmail.com>
2023-08-14 17:38:07 +0100

    optimized seo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
     <App />
-)
+  </HelmetProvider>
+);
